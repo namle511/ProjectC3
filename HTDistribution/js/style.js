77e1517ea6e4ebadb5587b915d298c4e0cdbc8d6
namle511 <42810183+namle511@users.noreply.github.com>
2019-06-17 02:17:49 +0700

    Add files via upload

---
+++ HTDistribution/js/style.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.demoslide img:gt(0)').hide();
+	setInterval(function(){
+      $('.demoslide :first-child').fadeOut() //FadeOut là ảnh đang hiện
+      .next('img').slideDown() //fadeIn ảnh tiếp theo
+      .end().appendTo('.demoslide'); // chuyển vị trí ảnh xuống cuối
+  	}, 3000);
+})
+
+
